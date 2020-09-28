61e595cd1c2d78d030c3f83d16f2f9d70064e347
alalalalaki <harlan.zhu@gmail.com>
2020-09-29 00:56:29 +0900

    Initial structure.

---
+++ estat/io/estat.py
@@ -0,0 +1,12 @@
+"""
+Read eStat data by using api
+Ref:
+https://www.e-stat.go.jp/api/api-info/api-spec
+https://www.e-stat.go.jp/api/api-info/e-stat-manual3-0
+"""
+
+import numpy as np
+import pandas as pd
+
+from pandas_datareader.base import _BaseReader
+
---
+++ estat/menu/estat.py
@@ -0,0 +1,4 @@
+"""
+Generate the menu of the data in eStat databse
+Ref: https://www.e-stat.go.jp/stat-search/database?page=1
+"""
